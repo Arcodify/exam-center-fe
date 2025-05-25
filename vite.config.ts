1ac1d8ce405acd6a4eb0c5ff693a4b622a744326
Rishab Shrestha <rishabshrestha92@gmail.com>
2025-05-25 10:33:33 +0545

    feat:Added Zustand

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import * as path from 'path';
+// import * as path from 'path'; // Not needed in Vite config for aliasing
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+    alias: [{ find: '@', replacement: '/src' }],
   },
 })
