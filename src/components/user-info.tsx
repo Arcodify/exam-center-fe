2f1619b2783d11cf49f7a4d8f917ec7d9f2bf6dd
bipinkatuwal567 <bipinkatuwal098@gmail.com>
2025-12-26 15:16:40 +0545

    add required dummy UI to the questions page

--- src/components/user-info.tsx
+++ src/components/user-info.tsx
@@ -16,37 +16,42 @@ const UserInfo = () => {
   };
 
   return (
-    <div className="bg-white/80 rounded-xl border border-white/30 p-5 w-full mx-auto">
-      <div className="flex flex-col items-start space-y-px">
+    <div className="bg-white/80 rounded-xl border border-white/30 w-full mx-auto mt-1 mb-0.5 pl-1">
+      <div className="flex gap-2 items-center">
         {/* Photo */}
-        <img
-          src={userInfo.photo}
-          alt="Student"
-          className="w-40 h-32 rounded-xl object-cover border-2 border-white mb-5"
-        />
 
-        <p className="text-slate-600">
-          <span className="font-medium">Name: </span>
-          <span className="font-bold">{userInfo.name}</span>
-        </p>
-        <p className="text-slate-600">
-          <span className="font-medium">Symbol No: </span>
-          <span className="">{userInfo.symbolNumber}</span>
-        </p>
-        {userInfo.subject && userInfo.subject.name && (
+        <div className="h-24 w-24">
+          <img
+            src={userInfo.photo}
+            alt="Student"
+            className="w-full h-full rounded-lg object-cover border-2 border-white"
+          />
+        </div>
+
+        <div>
           <p className="text-slate-600">
-            <span className="font-medium">Subject: </span>
-            <span className="">{userInfo.subject.name}</span>
+            <span className="font-medium">Name: </span>
+            <span className="font-bold">{userInfo.name}</span>
           </p>
-        )}
-        <p className="text-slate-600">
-          <span className="font-medium">Program: </span>
-          <span className="">{userInfo.program?.name}</span>
-        </p>
-        <p className="text-slate-600">
-          <span className="font-medium">Level: </span>
-          <span className="">{userInfo.level?.name}</span>
-        </p>
+          <p className="text-slate-600">
+            <span className="font-medium">Symbol No: </span>
+            <span className="">{userInfo.symbolNumber}</span>
+          </p>
+          {userInfo.subject && userInfo.subject.name && (
+            <p className="text-slate-600">
+              <span className="font-medium">Subject: </span>
+              <span className="">{userInfo.subject.name}</span>
+            </p>
+          )}
+          <p className="text-slate-600">
+            <span className="font-medium">Program: </span>
+            <span className="">{userInfo.program?.name}</span>
+          </p>
+          <p className="text-slate-600">
+            <span className="font-medium">Level: </span>
+            <span className="">{userInfo.level?.name}</span>
+          </p>
+        </div>
       </div>
     </div>
   );
